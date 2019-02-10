49ac362c47bd22c318073ac8a041514b76ae51c7
Rafael Castro <rafaelcg10@gmail.com>
2019-02-10 20:38:11 -0200

    more general file

---
+++ src/MoreGeneral.v
@@ -0,0 +1,33 @@
+Set Implicit Arguments.
+
+Require Import Unify.
+Require Import Schemes.
+Require Import Gen.
+Require Import List.
+Require Import ListIds.
+Require Import Context.
+Require Import Typing.
+Require Import Infer.
+Require Import NewTypeVariable.
+
+Require Import LibTactics.
+
+Inductive more_general : schm -> schm -> Prop :=
+    | more_general_intro : forall sigma1 sigma2 : schm,
+      (forall tau : ty, is_schm_instance tau sigma2 -> is_schm_instance tau sigma1) ->
+      more_general sigma1 sigma2.
+
+Inductive more_general_ctx : ctx -> ctx -> Prop :=
+  | more_general_ctx_nil : more_general_ctx nil nil
+  | more_general_ctx_cons : forall (G1 G2 : ctx) (i : id) (sigma1 sigma2 : schm),
+     more_general_ctx G1 G2 -> more_general sigma1 sigma2 ->
+     more_general_ctx ((i, sigma1) :: G1) ((i, sigma2) :: G2).
+
+Lemma typing_in_a_more_general_ctx : forall (e : term) (G2 G1 : ctx) (t : ty),
+    more_general_ctx G1 G2 -> has_type G2 e t -> has_type G1 e t.
+Admitted.
+
+Lemma more_general_gen_ty : forall (G1 G2 : ctx) (t : ty),
+    more_general_ctx G1 G2 -> more_general (gen_ty t G1) (gen_ty t G2).
+Admitted.
+
