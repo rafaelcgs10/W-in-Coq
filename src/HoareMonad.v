dc4268314bf910f437aa989de51951cc35380f84
Rafael Castro <rafaelcg10@gmail.com>
2019-03-03 13:33:33 -0300

    HoareState with option

--- src/HoareMonad.v
+++ src/HoareMonad.v
@@ -17,6 +17,9 @@ Definition Pre : Type := st -> Prop.
 
 Definition Post (a : Type) : Type := st -> a -> st -> Prop.
 
+Program Definition HoareState' (pre : Pre) (a : Set) (post : Post a) : Type :=
+forall i : {t : st | pre t}, {(x, f) : a * st | post i x f}.
+ 
 Program Definition HoareState (pre : Pre) (a : Set) (post : Post a) : Type :=
 forall i : sig (fun t : st => pre t), sig (fun anonymous : option (prod a st) => match anonymous with
                                                                     | Some (x, f) => post (proj1_sig i) x f
@@ -27,6 +30,35 @@ Definition top : Pre := fun st => True.
 
 Program Definition ret (a : Set) : forall x,
     @HoareState top a (fun i y f => i = f /\ y = x) := fun x s => exist _ (Some (x, s)) _.
+
+Program Definition bind' : forall a b P1 P2 Q1 Q2,
+  (@HoareState' P1 a Q1) -> (forall (x : a), @HoareState' (P2 x) b (Q2 x)) ->
+  @HoareState' (fun s1 => P1 s1 /\ forall x s2, Q1 s1 x s2 -> P2 x s2) b (fun s1 y s3 => exists x, exists s2, Q1 s1 x s2 /\ Q2 x s2 y s3) :=
+  fun a b P1 P2 Q1 Q2 c1 c2 s1 => match c1 s1 with (x, s2) => c2 x s2 end.
+Next Obligation.
+  cbv in Heq_anonymous.
+  destruct c1.
+  destruct x0.
+  inversion Heq_anonymous.
+  subst.
+  simpl in y.
+  auto.
+Defined.
+Next Obligation.
+  destruct (c2 x).
+  destruct x0.
+  cbv in Heq_anonymous.
+  destruct c1.
+  simpl in y.
+  simpl.
+  exists x s2.
+  split; auto.
+  destruct x0.
+  inversion Heq_anonymous.
+  subst.
+  simpl in y0.
+  auto.
+Defined.
 
 Program Definition bind : forall a b P1 P2 Q1 Q2,
   (@HoareState P1 a Q1) -> (forall (x : a), @HoareState (P2 x) b (Q2 x)) ->
@@ -48,33 +80,34 @@ Next Obligation.
   - simpl in y. contradiction.
 Defined.
 Next Obligation.
-  assert (P2 x s2) as HP2.
-  skip.
-  unfold HoareState in c2.
-  apply exist in HP2.
-  pose proof (c2 x HP2) as K.
-  destruct K.
+  destruct (c2 x).
+  destruct x0.
+  cbv in Heq_y.
+  simpl in *.
+  destruct p1.
+  exists x s2.
+  split;
+  auto.
+  destruct c1.
   destruct x0.
   destruct p1.
-  cbv in y.
-  destruct HP2.
-
-  Unset Printing Notations.
-
-  pose proof (proj1_sig (c2 x (exist (fun t : st => P2 x t) s2 (bind_obligation_2 Heq_y))) : (forall _ : eq anonymous' (proj1_sig (c2 x (exist (fun t : st => P2 x t) s2 (bind_obligation_2 Heq_y)))), Prop)).
-
-  pose proof (` (c2 x (exist (fun t : st => P2 x t) s2 (bind_obligation_2 Heq_y)))).
-
-  edestruct c2.
-  destruct ((c1 (exist (fun t : st => P1 t) s1 p))).
-  destruct x0.
-  destruct p1.
+  simpl in *.
   inversion Heq_y.
   subst.
+  auto.
+  inversion Heq_y.
+  simpl in *.
+  auto.
+Defined.
+Next Obligation.
+  cbv in Heq_y.
+  destruct c1.
   simpl in y.
-  destruct Heq_y.
-
-Admitted.
+  destruct x.
+  destruct p1.
+  inversion Heq_y.
+  auto.
+Defined.
 
 Program Definition get : @HoareState top st (fun i x f => i = f /\ x = i) := fun s => exist _ (Some (s, s)) _.
 
@@ -119,28 +152,41 @@ Next Obligation.
   split; auto.
 Defined.
 Next Obligation.
-  remember (fn x) as HH.
+  cbv.
   edestruct (fn x).
-  simpl in y.
-  destruct x1 in y.
-  destruct p in y.
-  destruct ((x' <- fn x; ret x')).
-
-  edestruct (fn x).
-  simpl in *.
   destruct x1.
-  destruct p.
   simpl in *.
+  destruct p.
+  auto.
+  simpl in y.
+  auto.
+Defined.
 
-  apply fn in x.
-  edestruct x.
+(*
+Lemma toListPost : forall A B, forall (post : Post A B), A -> (@HoareState A pre1 B pos1)) -> Post A (list B).
+  intros.
+  unfold Post in *.
+  intros.
+  eapply X; auto.
+Qed.
+*)
 
+Program Fixpoint mapMH (A B : Set) pre1 pos1 pre2 pos2
+        (fn : forall x : A, (@HoareState A (pre1 x) B (pos1 x))) (l : list A) : @HoareState A pre2 (list B) pos2 := 
   match l with
   | nil => ret nil
   | x::xs =>
     DO
       x' <- fn x ;
-      ret (x'::nil)
+      xs' <- mapMH pos2 fn xs ;
+      ret (x'::xs')
     OD
    end.
 Next Obligation.
+  unfold top.
+  auto.
+Defined.
+Next Obligation.
+  auto.
+  unfold HoareState in fn.
+  apply exist in H.
