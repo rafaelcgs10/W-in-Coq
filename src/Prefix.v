ede1dd0e9b5a1102d6b1c308bf50e264b19dedbd
Rafael Castro <rafaelcg10@gmail.com>
2019-05-07 19:48:52 -0300

    rename prefix

--- src/Prefix.v
+++ src/Prefix.v
@@ -14,15 +14,15 @@ Require Import Nth_error_tools.
 Require Import LibTactics.
 
 
-Inductive is_prefixe_free2 : list id -> list id -> list id -> Prop :=
+Inductive is_prefixe_free : list id -> list id -> list id -> Prop :=
 |  prefixe_free2_intro : forall C l L : list id,
-    {l1 : list id | L = l ++ l1 /\ are_disjoints C l1} -> is_prefixe_free2 C l L.
+    {l1 : list id | L = l ++ l1 /\ are_disjoints C l1} -> is_prefixe_free C l L.
 
-Hint Constructors is_prefixe_free2.
+Hint Constructors is_prefixe_free.
 
 Lemma is_prefixe2_gen_aux : forall (l L : list id) (tau : ty) (G : ctx),
-    is_prefixe_free2 (FV_ctx G) (snd (gen_ty_aux tau G l)) L ->
-    is_prefixe_free2 (FV_ctx G) l L.
+    is_prefixe_free (FV_ctx G) (snd (gen_ty_aux tau G l)) L ->
+    is_prefixe_free (FV_ctx G) l L.
 Proof.
   intros.
   destruct (exists_snd_gen_aux_app G tau l).
@@ -41,7 +41,7 @@ Qed.
 
 Hint Resolve is_prefixe2_gen_aux.
 
-Lemma is_prefixe_reflexivity : forall C L : list id, is_prefixe_free2 C L L.
+Lemma is_prefixe_reflexivity : forall C L : list id, is_prefixe_free C L L.
 intros C L.
 econstructor.
 exists (nil: list id).
